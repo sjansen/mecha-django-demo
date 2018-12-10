5e62f97f4dd8a2154cef504f9560cc98c257bf38
Stuart Jansen <sjansen@buscaluz.org>
2018-12-10 08:02:39 -0700

    add redirect from app root to chat

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
-urlpatterns = [path("chat/", include("app.chat.urls")), path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("", RedirectView.as_view(url="/chat/")),
+    path("chat/", include("app.chat.urls")),
+    path("admin/", admin.site.urls),
+]
